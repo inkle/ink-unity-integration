fe2fe5b16507a007ad44f7ffb0623e4bc3746612
Tom Kail <tkail92@gmail.com>
2016-07-08 15:12:33 +0100

    Add example

---
+++ Assets/Plugins/Ink/Example/Scripts/BasicInkExample.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using Ink.Runtime;
+
+public class BasicInkExample : MonoBehaviour {
+	[SerializeField]
+	private TextAsset inkJSONAsset;
+	private Story story;
+
+	[SerializeField]
+	private Canvas canvas;
+
+	// UI Prefabs
+	[SerializeField]
+	private Text textPrefab;
+	[SerializeField]
+	private Button buttonPrefab;
+
+	void Awake () {
+		StartStory();
+	}
+
+	void StartStory () {
+		story = new Story (inkJSONAsset.text);
+		RefreshView();
+	}
+
+	void RefreshView () {
+		RemoveChildren ();
+
+		while (story.canContinue) {
+			string text = story.Continue ().Trim();
+			CreateContentView(text);
+		}
+
+		if(story.currentChoices.Count > 0) {
+			for (int i = 0; i < story.currentChoices.Count; i++) {
+				Choice choice = story.currentChoices [i];
+				Button button = CreateChoiceView (choice.text.Trim ());
+				button.onClick.AddListener (delegate {
+					OnClickChoiceButton (choice);
+				});
+			}
+		} else {
+			Button choice = CreateChoiceView("End of story.\nRestart?");
+			choice.onClick.AddListener(delegate{
+				StartStory();
+			});
+		}
+	}
+
+	void OnClickChoiceButton (Choice choice) {
+		story.ChooseChoiceIndex (choice.index);
+		RefreshView();
+	}
+
+	void CreateContentView (string text) {
+		Text storyText = Instantiate (textPrefab) as Text;
+		storyText.text = text;
+		storyText.transform.SetParent (canvas.transform, false);
+	}
+
+	Button CreateChoiceView (string text) {
+		Button choice = Instantiate (buttonPrefab) as Button;
+		choice.transform.SetParent (canvas.transform, false);
+
+		Text choiceText = choice.GetComponentInChildren<Text> ();
+		choiceText.text = text;
+
+		HorizontalLayoutGroup layoutGroup = choice.GetComponent <HorizontalLayoutGroup> ();
+		layoutGroup.childForceExpandHeight = false;
+
+		return choice;
+	}
+
+	void RemoveChildren () {
+		int childCount = canvas.transform.childCount;
+		for (int i = childCount - 1; i >= 0; --i) {
+			GameObject.Destroy (canvas.transform.GetChild (i).gameObject);
+		}
+	}
+}
