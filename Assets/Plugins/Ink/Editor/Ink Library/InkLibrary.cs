af82c56526b4e9f66c5ee16bc2d9317c9dc0dad3
Tom Kail <tkail92@gmail.com>
2017-07-05 17:32:50 +0100

    Removed unused namespace reference

--- Assets/Plugins/Ink/Editor/Ink Library/InkLibrary.cs
+++ Assets/Plugins/Ink/Editor/Ink Library/InkLibrary.cs
@@ -5,7 +5,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Diagnostics;
-using UnityEditorInternal;
 using Debug = UnityEngine.Debug;
 using Ink.Runtime;
 
