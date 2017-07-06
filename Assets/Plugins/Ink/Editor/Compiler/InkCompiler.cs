1044021fe359bead59bd952fb2a79a3280ebbb48
Tom Kail <tkail92@gmail.com>
2017-07-06 10:58:01 +0100

    Ensure the progress bar clears if rare bug I’ve not pinned down occurs. Seems to have something to do with crashes during the importing stage?

--- Assets/Plugins/Ink/Editor/Compiler/InkCompiler.cs
+++ Assets/Plugins/Ink/Editor/Compiler/InkCompiler.cs
@@ -57,18 +57,29 @@ namespace Ink.UnityIntegration {
 
 			for (int i = InkLibrary.Instance.compilationStack.Count - 1; i >= 0; i--) {
 				var compilingFile = InkLibrary.Instance.compilationStack [i];
-				if (compilingFile.state != CompilationStackItem.State.Compiling)
-					continue;
-				
-				compilingFile.timeTaken = (float)((DateTime.Now - compilingFile.startTime).TotalSeconds);
-				if (compilingFile.timeTaken > InkSettings.Instance.compileTimeout) {
-					if (compilingFile.process != null) {	
-						compilingFile.process.Exited -= OnCompileProcessComplete;
-						compilingFile.process.Kill ();
+				if (compilingFile.state == CompilationStackItem.State.Compiling) {
+					compilingFile.timeTaken = (float)((DateTime.Now - compilingFile.startTime).TotalSeconds);
+					if (compilingFile.timeTaken > InkSettings.Instance.compileTimeout) {
+						if (compilingFile.process != null) {	
+							compilingFile.process.Exited -= OnCompileProcessComplete;
+							compilingFile.process.Kill ();
+						}
+						InkLibrary.Instance.compilationStack.RemoveAt(i);
+						if(InkLibrary.Instance.compilationStack.Count == 0) EditorUtility.ClearProgressBar();
+						Debug.LogError("Ink Compiler timed out for "+compilingFile.inkAbsoluteFilePath+".\n. Compilation should never take more than a few seconds, but for large projects or slow computers you may want to increase the timeout time in the InkSettings file.\nIf this persists there may be another issue; or else check an ink file exists at this path and try Assets/Recompile Ink, else please report as a bug with the following error log at this address: https://github.com/inkle/ink/issues\nError log:\n"+compilingFile.errorOutput);
 					}
-					InkLibrary.Instance.compilationStack.RemoveAt(i);
-					EditorUtility.ClearProgressBar();
-					Debug.LogError("Ink Compiler timed out for "+compilingFile.inkAbsoluteFilePath+".\n. Compilation should never take more than a few seconds, but for large projects or slow computers you may want to increase the timeout time in the InkSettings file.\nIf this persists there may be another issue; or else check an ink file exists at this path and try Assets/Recompile Ink, else please report as a bug with the following error log at this address: https://github.com/inkle/ink/issues\nError log:\n"+compilingFile.errorOutput);
+				} else if (compilingFile.state == CompilationStackItem.State.Importing) {
+					// This covers a rare bug that I've not pinned down
+					var timeTaken = (float)((DateTime.Now - compilingFile.startTime).TotalSeconds);
+					if (timeTaken > InkSettings.Instance.compileTimeout + 2) {
+						if (compilingFile.process != null) {	
+							compilingFile.process.Exited -= OnCompileProcessComplete;
+							compilingFile.process.Kill ();
+						}
+						InkLibrary.Instance.compilationStack.RemoveAt(i);
+						if(InkLibrary.Instance.compilationStack.Count == 0) EditorUtility.ClearProgressBar();
+						Debug.LogError("Ink Compiler timed out for "+compilingFile.inkAbsoluteFilePath+" while the file was importing.\n. Please report as a bug with the following error log at this address: https://github.com/inkle/ink/issues\nError log:\n"+compilingFile.errorOutput);
+					}
 				}
 			}
 			if(InkLibrary.Instance.compilationStack.Count > 0) {
