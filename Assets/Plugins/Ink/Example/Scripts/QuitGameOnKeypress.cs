a7bb017b339bfa926907a74e1f128689b041f135
Tom <tom.kail@inklestudios.com>
2019-03-11 14:46:00 +0000

    add quit key for builds

---
+++ Assets/Plugins/Ink/Example/Scripts/QuitGameOnKeypress.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuitGameOnKeypress : MonoBehaviour {
+	
+	public KeyCode key = KeyCode.Escape;
+	
+	void Update () {
+		if(Input.GetKeyDown(key)) Application.Quit();
+	}
+}
