5566d42ad0880228d36e0b535c649d7a9d39088d
Tom <tom.kail@inklestudios.com>
2019-07-26 13:13:35 +0100

    Caches a variable in InkFile for performance

--- Assets/Plugins/Ink/Editor/Ink Library/InkFile.cs
+++ Assets/Plugins/Ink/Editor/Ink Library/InkFile.cs
@@ -58,6 +58,9 @@ namespace Ink.UnityIntegration {
 		// The path of any compiled json file. Relative to assets folder.
         public string jsonPath {
 			get {
+                var _filePath = filePath;
+                Debug.Assert(_filePath != null, "File path for ink file is null! The ink library requires rebuilding.");
+
                 DefaultAsset jsonFolder = jsonAssetPath;
                 if (jsonFolder == null) // no path specified for this specific file
                 {
@@ -70,14 +73,15 @@ namespace Ink.UnityIntegration {
                     if (jsonFolder == null)
                     {
                         //fallback to same folder as .ink file
-                        jsonFolder = AssetDatabase.LoadAssetAtPath<DefaultAsset>(Path.GetDirectoryName(filePath));
+                        jsonFolder = AssetDatabase.LoadAssetAtPath<DefaultAsset>(Path.GetDirectoryName(_filePath));
                     }
                 }
 
-                string jsonPath = AssetDatabase.GetAssetPath(jsonFolder);
-                string strJsonAssetPath = InkEditorUtils.CombinePaths(jsonPath, Path.GetFileNameWithoutExtension(filePath)) + ".json";
+                Debug.Assert(jsonFolder != null, "JSON folder not found for ink file at path "+_filePath);
 
-                return InkEditorUtils.SanitizePathString(strJsonAssetPath);
+                string jsonPath = AssetDatabase.GetAssetPath(jsonFolder);
+                string strJsonAssetPath = InkEditorUtils.CombinePaths(jsonPath, Path.GetFileNameWithoutExtension(_filePath)) + ".json";
+                return strJsonAssetPath;
 			}
 		}
 
