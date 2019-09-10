61944ef1125923930b0915287856d607576b86ff
Tom <tom.kail@inklestudios.com>
2019-09-10 18:02:37 +0100

    Allows attached stories to fully output in the player window, and provides an example of auto-attaching

--- Assets/Plugins/Ink/Example/Scripts/Editor/BasicInkExampleEditor.cs
+++ Assets/Plugins/Ink/Example/Scripts/Editor/BasicInkExampleEditor.cs
@@ -3,9 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 using Ink.UnityIntegration;
+using Ink.Runtime;
 
 [CustomEditor(typeof(BasicInkExample))]
 public class BasicInkExampleEditor : Editor {
+
+    static BasicInkExampleEditor () {
+        BasicInkExample.OnCreateStory += OnCreateStory;
+    }
+
+    static void OnCreateStory (Story story) {
+        // If you'd like NOT to automatically show the window and attach (your teammates may appreciate it!) then replace "true" with "false" here. 
+        InkPlayerWindow window = InkPlayerWindow.GetWindow(true);
+        if(window != null) window.AttachInstance(story);
+    }
 	public override void OnInspectorGUI () {
 		Repaint();
 		base.OnInspectorGUI ();
