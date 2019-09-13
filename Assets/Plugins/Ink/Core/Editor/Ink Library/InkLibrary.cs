a3f0a598ee5e9e19f5f7b26a4d920d5cdf4af9bc
harryr0se <harry.rose1993@gmail.com>
2019-09-13 15:55:27 +0100

    Update compilation process to use the ink compiler directly Previously an external process would be kicked off, the InkCompiler class will now farm off the compilation to a threaded job. This change will need thorough testing, but from my local testing features such as error/warning tracking and delaying enter playmode are all functioning correctly.

--- Assets/Plugins/Ink/Core/Editor/Ink Library/InkLibrary.cs
+++ Assets/Plugins/Ink/Core/Editor/Ink Library/InkLibrary.cs
@@ -221,15 +221,6 @@ namespace Ink.UnityIntegration {
 			return items;
 		}
 
-		public static InkCompiler.CompilationStackItem GetCompilationStackItem (Process process) {
-			foreach(var x in Instance.compilationStack) {
-				if(x.process == process) 
-					return x;
-			}
-			Debug.LogError("Fatal Error compiling Ink! No file found! Please report this as a bug. "+process);
-			return null;
-		}
-
 		public static InkCompiler.CompilationStackItem GetCompilationStackItem (InkFile inkFile) {
 			foreach(var x in Instance.compilationStack) {
 				if(x.inkFile == inkFile) 
