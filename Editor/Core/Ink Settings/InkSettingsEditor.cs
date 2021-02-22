ee1fde7a582847a280ccd3900529810ee232196c
Tom Kail <tkail92@gmail.com>
2021-02-22 15:12:25 +0000

    Improved ink version label

--- Editor/Core/Ink Settings/InkSettingsEditor.cs
+++ Editor/Core/Ink Settings/InkSettingsEditor.cs
@@ -46,11 +46,12 @@ namespace Ink.UnityIntegration {
 		}
 		#endif
 
+        static string versionLabel => string.Format("Ink Unity Integration version "+InkLibrary.unityIntegrationVersionCurrent+"\nInk version "+InkLibrary.inkVersionCurrent+"\nInk story format version "+Ink.Runtime.Story.inkVersionCurrent+"\nInk save format version "+Ink.Runtime.StoryState.kInkSaveStateVersion);
 		static void DrawSettings (InkSettings settings) {
 			var cachedLabelWidth = EditorGUIUtility.labelWidth;
 			EditorGUIUtility.labelWidth = 260;
 
-			EditorGUILayout.HelpBox("Ink Unity Integration version "+InkLibrary.versionCurrent+"\nInk Story Version "+Ink.Runtime.Story.inkVersionCurrent, MessageType.Info);
+			EditorGUILayout.HelpBox(versionLabel, MessageType.Info);
 
 			if(settings.templateFile == null) {
 				EditorGUILayout.HelpBox("Template not found. Ink files created via Assets > Create > Ink will be blank.", MessageType.Info);
@@ -64,13 +65,18 @@ namespace Ink.UnityIntegration {
 			settings.compileTimeout = EditorGUILayout.IntField(new GUIContent("Compile Timeout", "The max time the compiler will attempt to compile for in case of unhanded errors. You may need to increase this for very large ink projects."), settings.compileTimeout);
 
 			EditorGUIUtility.labelWidth = cachedLabelWidth;
+
+            EditorGUILayout.Separator();
+            if(GUILayout.Button("Show changelog")) {
+                InkUnityIntegrationStartupWindow.ShowWindow();
+            }
 		}
 		static void DrawSettings (SerializedObject settings) {
 			var cachedLabelWidth = EditorGUIUtility.labelWidth;
 			EditorGUIUtility.labelWidth = 260;
 			EditorGUI.BeginChangeCheck();
 
-			EditorGUILayout.HelpBox("Ink Unity Integration version "+InkLibrary.versionCurrent+"\nInk Story Version "+Ink.Runtime.Story.inkVersionCurrent, MessageType.Info);
+			EditorGUILayout.HelpBox(versionLabel, MessageType.Info);
 
 			if(settings.FindProperty("templateFile").objectReferenceValue == null) {
 				EditorGUILayout.HelpBox("Template not found. Ink files created via Assets > Create > Ink will be blank.", MessageType.Info);
@@ -82,10 +88,16 @@ namespace Ink.UnityIntegration {
             EditorGUILayout.PropertyField(settings.FindProperty("printInkLogsInConsoleOnCompile"), new GUIContent("Print ink TODOs in console on compile", "When enabled, ink lines starting with TODO are printed in the console."));
             EditorGUILayout.PropertyField(settings.FindProperty("handleJSONFilesAutomatically"), new GUIContent("Handle JSON Automatically", "Whether JSON files are moved, renamed and deleted along with their ink files."));
 			EditorGUILayout.PropertyField(settings.FindProperty("compileTimeout"), new GUIContent("Compile Timeout", "The max time the compiler will attempt to compile for in case of unhanded errors. You may need to increase this for very large ink projects."));
+            
             if(EditorGUI.EndChangeCheck()) {
 				settings.ApplyModifiedProperties();
 			}
 			EditorGUIUtility.labelWidth = cachedLabelWidth;
+            
+            EditorGUILayout.Separator();
+            if(GUILayout.Button("Show changelog")) {
+                InkUnityIntegrationStartupWindow.ShowWindow();
+            }
 		}
 	}
 }
