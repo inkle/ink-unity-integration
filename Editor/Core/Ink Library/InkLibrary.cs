66fcef5f41e24de7aab2d21e608ba577b58a457e
Tom Kail <thomas.kail@betterup.co>
2022-02-16 15:10:11 +0000

    Upgrade version to 1.0.3

--- Editor/Core/Ink Library/InkLibrary.cs
+++ Editor/Core/Ink Library/InkLibrary.cs
@@ -21,7 +21,7 @@ namespace Ink.UnityIntegration {
     #endif
         // Ink version. This should really come from the core ink code.
 		public static System.Version inkVersionCurrent = new System.Version(1,0,0);
-		public static System.Version unityIntegrationVersionCurrent = new System.Version(1,0,2);
+		public static System.Version unityIntegrationVersionCurrent = new System.Version(1,0,3);
 
 		static string absoluteSavePath {
 			get {
