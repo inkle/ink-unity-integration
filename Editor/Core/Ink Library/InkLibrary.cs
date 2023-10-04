839412ea8221df738e8677fadddb1b1fc6e2c21f
Tom Kail <thomas.kail@betterup.co>
2023-10-04 10:17:37 +0100

    Removes some upgrade code from 0.9.71. It's unlikely that anyone will upgrade from such an old version and I'd like to clean the code up. Not having this code won't wreck anything for those who do.

--- Editor/Core/Ink Library/InkLibrary.cs
+++ Editor/Core/Ink Library/InkLibrary.cs
@@ -116,21 +116,6 @@ namespace Ink.UnityIntegration {
                 Validate();
             };
         }
-		// After recompile, the data associated with the object is fetched (or whatever happens to it) by this point. 
-		void OnEnable () {
-			// Deletes the persistent version of this asset that we used to use prior to 0.9.71
-			if(!Application.isPlaying && EditorUtility.IsPersistent(this)) {
-				var path = AssetDatabase.GetAssetPath(this);
-				if(!string.IsNullOrEmpty(path)) {
-					#if !UNITY_2020_1_OR_NEWER
-                    if(_instance == this) _instance = null;
-					#endif
-					AssetDatabase.DeleteAsset(path);
-					AssetDatabase.Refresh();
-					return;
-				}
-			}
-		}
 
         static void BuildLookupDictionary () {
             instance.inkLibraryDictionary.Clear();
