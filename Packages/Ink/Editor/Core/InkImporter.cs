2137a7d50628d53afbb1aafc9751a8bb4d6569db
Alex Larioza <contact@gamesrightmeow.com>
2025-08-28 06:49:01 -0700

    move importer into ink.unityintegration namespace

--- Packages/Ink/Editor/Core/InkImporter.cs
+++ Packages/Ink/Editor/Core/InkImporter.cs
@@ -5,80 +5,91 @@ using Ink.UnityIntegration;
 using UnityEditor;
 using Ink;
 
-/// <summary>
-/// Automatically compiles .ink assets each time they are imported, and creates an InkFile asset.
-/// </summary>
-[ScriptedImporter(1, "ink")]
-public class InkImporter : ScriptedImporter
+namespace Ink.UnityIntegration
 {
-    [SerializeField]
-    [Tooltip("Set this to false to stop the Ink file from being compiled on import. This is intended for " +
-    "Ink files that aren't intended to be compiled as standalone files, because they are included in other " + 
-    "Ink files and require other files to be compiled correctly e.g. global variables are defined in another file.")]
-    private bool compileOnImport = true;
-
-    public override void OnImportAsset(AssetImportContext ctx)
+    /// <summary>
+    /// Automatically compiles .ink assets each time they are imported, and creates an InkFile asset.
+    /// </summary>
+    [ScriptedImporter(1, "ink")]
+    public class InkImporter : ScriptedImporter
     {
-        if (!compileOnImport) 
-        {
-            return;
-        }
+        [SerializeField]
+        [Tooltip("Set this to false to stop the Ink file from being compiled on import. This is intended for " +
+        "Ink files that aren't intended to be compiled as standalone files, because they are included in other " +
+        "Ink files and require other files to be compiled correctly e.g. global variables are defined in another file.")]
+        private bool compileOnImport = true;
 
-        var inkFile = ScriptableObject.CreateInstance<InkFile>();
-        var absolutePath = InkEditorUtils.UnityRelativeToAbsolutePath(ctx.assetPath);
-        var inputString = File.ReadAllText(ctx.assetPath);
-        var compiler = new Compiler(inputString, new Compiler.Options
+        public override void OnImportAsset(AssetImportContext ctx)
         {
-            countAllVisits = true,
-            fileHandler = new UnityInkFileHandler(Path.GetDirectoryName(absolutePath)),
-            errorHandler = (string message, ErrorType type) => {
-                InkCompilerLog log;
-                if(InkCompilerLog.TryParse(message, out log)) {
-                    if(string.IsNullOrEmpty(log.relativeFilePath)) log.relativeFilePath = Path.GetFileName(absolutePath);
-                    switch (log.type)
-                    {
-                        case ErrorType.Error: 
-                            inkFile.errors.Add(log); 
-                            Debug.LogError("Ink "+log.type+" for "+Path.GetFileName(absolutePath)+": "+log.content + " (at "+log.relativeFilePath+":"+log.lineNumber+")", inkFile);
-                            break;
-                        case ErrorType.Warning: 
-                            inkFile.warnings.Add(log); 
-                            Debug.LogWarning("Ink "+log.type+" for "+Path.GetFileName(absolutePath)+": "+log.content + " (at "+log.relativeFilePath+" "+log.lineNumber+")", inkFile);
-                            break;
-                        case ErrorType.Author: 
-                            inkFile.todos.Add(log);
-                            if (InkSettings.instance.printInkLogsInConsoleOnCompile)
-                            {
-                                Debug.Log("Ink Log for "+Path.GetFileName(absolutePath)+": "+log.content + " (at "+log.relativeFilePath+" "+log.lineNumber+")", inkFile);
-                            }
-                            break;
-                    }
-                    
-                } else {
-                    Debug.LogWarning("Couldn't parse log "+message);
-                }
+            if (!compileOnImport)
+            {
+                return;
             }
-        });
-        
-        try {
-            var compiledStory = compiler.Compile();
 
-            if (compiledStory != null)
-                inkFile.Initialize(compiledStory.ToJson());
-        } catch (System.SystemException e) {
-            Debug.LogException(e);
+            var inkFile = ScriptableObject.CreateInstance<InkFile>();
+            var absolutePath = InkEditorUtils.UnityRelativeToAbsolutePath(ctx.assetPath);
+            var inputString = File.ReadAllText(ctx.assetPath);
+            var compiler = new Compiler(inputString, new Compiler.Options
+            {
+                countAllVisits = true,
+                fileHandler = new UnityInkFileHandler(Path.GetDirectoryName(absolutePath)),
+                errorHandler = (string message, ErrorType type) =>
+                {
+                    InkCompilerLog log;
+                    if (InkCompilerLog.TryParse(message, out log))
+                    {
+                        if (string.IsNullOrEmpty(log.relativeFilePath)) log.relativeFilePath = Path.GetFileName(absolutePath);
+                        switch (log.type)
+                        {
+                            case ErrorType.Error:
+                                inkFile.errors.Add(log);
+                                Debug.LogError("Ink " + log.type + " for " + Path.GetFileName(absolutePath) + ": " + log.content + " (at " + log.relativeFilePath + ":" + log.lineNumber + ")", inkFile);
+                                break;
+                            case ErrorType.Warning:
+                                inkFile.warnings.Add(log);
+                                Debug.LogWarning("Ink " + log.type + " for " + Path.GetFileName(absolutePath) + ": " + log.content + " (at " + log.relativeFilePath + " " + log.lineNumber + ")", inkFile);
+                                break;
+                            case ErrorType.Author:
+                                inkFile.todos.Add(log);
+                                if (InkSettings.instance.printInkLogsInConsoleOnCompile)
+                                {
+                                    Debug.Log("Ink Log for " + Path.GetFileName(absolutePath) + ": " + log.content + " (at " + log.relativeFilePath + " " + log.lineNumber + ")", inkFile);
+                                }
+                                break;
+                        }
+
+                    }
+                    else
+                    {
+                        Debug.LogWarning("Couldn't parse log " + message);
+                    }
+                }
+            });
+
+            try
+            {
+                var compiledStory = compiler.Compile();
+
+                if (compiledStory != null)
+                    inkFile.Initialize(compiledStory.ToJson());
+            }
+            catch (System.SystemException e)
+            {
+                Debug.LogException(e);
+            }
+
+            /** inform Unity about dependencies defined in ink files so that if included
+            files are modified, the main file is also reimported.**/
+            var includes = InkFile.InkIncludeParser.ParseIncludes(inputString);
+            foreach (var include in includes)
+            {
+                // Unity wants the path relative to the Assets folder
+                var assetPath = Path.Combine(Path.GetDirectoryName(ctx.assetPath), include);
+                ctx.DependsOnSourceAsset(assetPath);
+            }
+
+            ctx.AddObjectToAsset("InkFile", inkFile);
+            ctx.SetMainObject(inkFile);
         }
-
-        /** inform Unity about dependencies defined in ink files so that if included
-        files are modified, the main file is also reimported.**/
-        var includes = InkFile.InkIncludeParser.ParseIncludes(inputString);
-        foreach (var include in includes) {
-            // Unity wants the path relative to the Assets folder
-            var assetPath = Path.Combine(Path.GetDirectoryName(ctx.assetPath), include);
-            ctx.DependsOnSourceAsset(assetPath);
-        }
-
-        ctx.AddObjectToAsset("InkFile", inkFile);
-        ctx.SetMainObject(inkFile);
     }
 }
